7eeee39433caff3feea321e385ba10f5dc90739e
Zoe Parys <zoe.parys@kearsarge.org>
2014-12-15 13:00:20 -0500

    added "GAme" (wall code)

--- Game.py
+++ Game.py
@@ -2,3 +2,13 @@ import pygame
 from Wall import Wall
 pygame.init()
 
+
+#import pygame, math
+
+#class Wall():
+	#def __init__(self, tl, br):
+	#	self.image = pygame.image.load("Resources/Objects/Wall/Wall.png")
+	#	width = br[0] - tl[0]
+	#	height = br[1] - tl[1]
+	#	self.image = pygame.transform.scale(self.image, [width, height])	
+	#	self.rect = self.image.get_rect(topleft = tl)
